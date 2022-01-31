25c9df3ee86225f4130d59d45cecadfd05f2b4fa
Diego Vanderlei da Silva Hoffmann <diegohoffmann4@gmail.com>
2022-01-31 15:37:54 +0100

    Customizacao
    Customizacao do formulario e botoes

---
+++ projetos/src/js/cuestio.js
@@ -0,0 +1,18 @@
+function mira_datos(){
+    let nombre = document.getElementById('nombre').value
+    let apellido = document.getElementById('apellido').value
+    let email = document.getElementById('mail').value
+    let fecha = document.getElementById('Dnasci').value
+
+    let ver = window.open("", "visualizar", "width=280, height=600")
+        ver.document.write("<html>")
+        ver.document.write("<head><link rel='stylesheet' href='../projetos/src/css/est_cuestio.css'></head>")
+        ver.document.write("<body><h2>Sus Datos</h2>")
+        ver.document.write("<div class='datos_part1'><p>" + nombre + " " + apellido + "</p></div>")
+        ver.document.write("<p>" + email + '</p><br>')
+        ver.document.write('<p>' + fecha + '</p>')
+        ver.document.write("</body></html>")
+        ver.document.close()
+}
+
+
