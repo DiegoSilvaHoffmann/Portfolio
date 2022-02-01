3c524b1ed0d4eb7fad3b97181d431c8c93826c2f
Diego Vanderlei da Silva Hoffmann <diegohoffmann4@gmail.com>
2022-02-01 10:55:50 +0100

    Buttons
    Elaborado botones, y estilo de la pagina nueva

--- projetos/src/js/cuestio.js
+++ projetos/src/js/cuestio.js
@@ -3,14 +3,19 @@ function mira_datos(){
     let apellido = document.getElementById('apellido').value
     let email = document.getElementById('mail').value
     let fecha = document.getElementById('Dnasci').value
+    let direc = document.getElementById('direccion').value
+    let city = document.getElementById('ciudad').value
 
-    let ver = window.open("", "visualizar", "width=280, height=600")
+    let ver = window.open("", "visualizar", "width=320, height=600")
         ver.document.write("<html>")
         ver.document.write("<head><link rel='stylesheet' href='../projetos/src/css/est_cuestio.css'></head>")
         ver.document.write("<body><h2>Sus Datos</h2>")
-        ver.document.write("<div class='datos_part1'><p>" + nombre + " " + apellido + "</p></div>")
-        ver.document.write("<p>" + email + '</p><br>')
-        ver.document.write('<p>' + fecha + '</p>')
+        ver.document.write("<div class='datos_part1'><p>" + nombre + " " + apellido + "</p>")
+        ver.document.write("<p>" + fecha + '</p><br>')
+        ver.document.write("<p>" + direc + "</p>")
+        ver.document.write("<p>" + city + "</p>")
+        ver.document.write('<p>' + email + '</p></div>')
+
         ver.document.write("</body></html>")
         ver.document.close()
 }
