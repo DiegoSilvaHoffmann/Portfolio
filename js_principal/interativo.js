048576e35d70e75c94bf1f9e0eda11684c662c38
Diego Vanderlei da Silva Hoffmann <diegohoffmann4@gmail.com>
2022-02-02 22:00:31 +0100

    Reveal

--- js_principal/interativo.js
+++ js_principal/interativo.js
@@ -4,11 +4,11 @@ function aparecer(){
     origin: 'center',
     duration: 3500,
     delay: 200,
-})
-desliza.reveal(".meu_nome", {origin: "center"})
-desliza.reveal(".menu", {origin: "left", distance: "160px"})
-desliza.reveal(".tecnologias", {origin: "center"})
-desliza.reveal("#ex_1", {origin: "left", distance: "160px"})
-desliza.reveal("#ex_2", {origin: "bottom", distance: "160px"})
-desliza.reveal("#ex_3", {origin: "right", distance: "160px"})
+  })
+  desliza.reveal(".meu_nome", {origin: "center"})
+  desliza.reveal(".menu", {origin: "left", distance: "160px"})
+  desliza.reveal(".tecnologias", {origin: "center"})
+  desliza.reveal("#ex_1", {origin: "left", distance: "160px"})
+  desliza.reveal("#ex_2", {origin: "bottom", distance: "160px"})
+  desliza.reveal("#ex_3", {origin: "right", distance: "160px"})
 }
